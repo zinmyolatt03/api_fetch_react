9201f6bae66349e6598facff8ff4513481516ea8
ZIN MYO LATT <myolatt@ZINs-MacBook-Air.local>
2025-06-20 14:14:08 +0630

    using custom hook

--- src/components/Triplist/triplist.js
+++ src/components/Triplist/triplist.js
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import useFetchApi from '../../hooks/useFetchApi';
 import './triplist.css'
 
 export default function Triplist() {
 
-    let [ trips,setTrips ] = useState(null);
-    let [ url , setUrl ] = useState("http://localhost:3001/trips");
-
-    useEffect( () => {
-        fetch(url)
-        .then( response => response.json())
-        .then( trips => {
-            setTrips( trips );
-        })
-    }, [ url ])
-
+    let [ url , setUrl ] = useState("http://localhost:3001/trips");    
+    let { data : trips } = useFetchApi(url);
+    
     const findTrips = () => {
         setUrl("http://localhost:3001/trips?country=Myanmar");
     }
+
 
 
     return (
